e1643ec38d88ed7ae954425269d24a28b628aae6
angel <paz.l.angel@gmail.com>
2025-04-19 13:48:28 -0300

    aula3

--- src/Lectura.java
+++ src/Lectura.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Lectura {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
---
+++ src/Loops.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double nota = 0;
+        double mediaEvaluaciones = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Ingrese la calificación de la película Matrix: ");
+            nota = teclado.nextDouble();
+            mediaEvaluaciones = mediaEvaluaciones + nota;
+            
+        }
+        System.out.println("La media de evaluaciones paraMatrix es:" + mediaEvaluaciones / 3);
+
+    }
+}
